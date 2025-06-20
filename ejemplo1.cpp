1c447eb6a08093f9fab1a4339f1dc9cc82f12eee
CarlosSandoval2025 <carlos.j98232s.s@gmail.com>
2025-06-19 20:34:30 -0500

    Mejora para obtener 10% por superar 100 U vendidas

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -21,13 +21,27 @@ int main(){
         cout<<"Ingrese el nombre del verndedor: ";
         cin>>EMPLEADOS[i].nom;
         cout<<"Cantidad de ventas: "<<endl; 
+        EMPLEADOS[i].S_Ven = 0;
         for(int j=0; j<12;j++){
             cout<<"Mes "<<j+1<<" : ";
             cin>>EMPLEADOS[i].ven[j];
             EMPLEADOS[i].S_Ven=EMPLEADOS[i].S_Ven + EMPLEADOS[i].ven[j];
+            
+        
         }
         cout<<"Ingrese el salario: "; 
         cin>>EMPLEADOS[i].sal;
+        
+        int Nu_sal;
+        if(EMPLEADOS[i].S_Ven>100){
+            Nu_sal=EMPLEADOS[i].sal+(EMPLEADOS[i].sal*0.10);
+            cout<<"El empleado N: "<<EMPLEADOS[i].num<<endl;
+            cout<<"------por superar el 100 unidades de ventas------"<<endl; 
+            cout<<"su salario incremento a: "<<Nu_sal<<endl;
+        }
+        else{
+            cout<<"No supero las 100 unidades de ventas"<<endl;
+        }   
     }
     int Val=0;
         float NV_Max=EMPLEADOS[0].S_Ven; 
@@ -37,10 +51,9 @@ int main(){
                 Val=i; 
             }
         }
-
     cout<<"---------ENTONCES EL EMPLEADO CON MAYOR VENTAS ANUALES---------"<<endl;
     cout<<"El empleado N: "<<EMPLEADOS[Val].num<<endl; 
     cout<<"De Nombre: "<<EMPLEADOS[Val].nom<<endl; 
-    cout<<"Tuvo anualmente un total de "<<EMPLEADOS[Val].S_Ven<<endl; 
+    cout<<"Tuvo anualmente un total de "<<EMPLEADOS[Val].S_Ven<<" Ventas anuales"<<endl; 
     return 0; 
 }
